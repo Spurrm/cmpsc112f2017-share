6a93e3f10dbd1246e5eb5a16e0ccf823b255e901
amohan <amohan@aldenv7.allegheny.edu>
2017-08-31 11:38:43 -0400

    Initial commit

---
+++ labs/code/Hooray.java
@@ -0,0 +1,15 @@
+public class Hooray {
+
+    public static void hooray() {
+
+		while(true) {
+			System.out.println("Hooray!");
+    	} //while
+
+    } //hooray
+
+    public static void main(String[] args) {
+		hooray();
+    } //main
+
+} //Hooray (class)
---
+++ labs/code/Weeee.java
@@ -0,0 +1,12 @@
+public class Weeee {
+
+    public static void weeee() {
+		System.out.println("Weeee!");
+		weeee();
+    } //weeee
+
+    public static void main(String[] args) {
+		weeee();
+    } //main
+
+} //Weeee (class)
